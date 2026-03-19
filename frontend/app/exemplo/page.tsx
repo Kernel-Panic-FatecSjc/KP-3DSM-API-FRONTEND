cef4294ef1870184f0138824891b6f88d4a6cfad
Vitor Serpa <vitorserpa123@gmail.com>
2026-03-19 18:52:13 -0300

    feat(not-US): Estruturação do padrão de pastas do frontend

---
+++ frontend/app/exemplo/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Exemplo de pagina</h1>
+    </div>
+  );
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>MAIN PAGE</h1>
+    </div>
+  );
+}
