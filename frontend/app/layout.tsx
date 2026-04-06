2a3867afdaf7b2e3b1aa6527499f57c6e1ab9d71
Vinícius Leite <contato.vinips25@outlook.com>
2026-04-05 21:20:25 -0300

    feat(not-US): Arrumando para que a navbar não apareça na página de login

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,17 +1,28 @@
+'use client';
+
 import "./globals.css";
 import NavigationBar from "@/components/layout/navegationBar/navegationBar";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+
+  const pathname = usePathname();
+
+  const hideNavbar = pathname === "/login";
+
   return (
     <html lang="pt-br">
       <body>
-        <header style={{ position: 'relative', zIndex: 9999 }}>
-          <NavigationBar />
-        </header>
+        {!hideNavbar && (
+          <header style={{ position: 'relative', zIndex: 9999 }}>
+            <NavigationBar />
+          </header>
+        )}
+
         <main className="main-wrapper">
           {children}
         </main>
