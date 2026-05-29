41442584c309727e1dfa5ff1d711c48813fd9a0d
Vinícius Leite <contato.vinips25@outlook.com>
2026-05-28 22:50:06 -0300

    chore(not-us): Arrumando o titulo das paginas

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import "./globals.css";
 import NavigationBar from "@/components/layout/navegationBar/navegationBar";
 import { usePathname } from "next/navigation";
@@ -9,20 +8,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   const pathname = usePathname();
-
   const hideNavbar = pathname === "/login";
 
   return (
     <html lang="pt-br">
+      <head>
+        <title>GSW Soluções Integradas</title>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
       <body>
         {!hideNavbar && (
           <header style={{ position: 'relative', zIndex: 9999 }}>
             <NavigationBar />
           </header>
         )}
-
         <main className="main-wrapper">
           {children}
         </main>
